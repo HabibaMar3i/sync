23dcd0fe8a609225d43e46bf598e2a4330122b52
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-17 22:29:10 +0300

    added fontawesome setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1>Test App</h1>
+      <h1 class="text-3xl font-bold underline">
+        Test Tailwind
+      </h1>
+      <i class="fa-solid fa-house"></i>
     </>
   )
 }
