8ac8491298dd5a5674a5def694e0bf0afda41aa0
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-18 01:48:37 +0300

    added small setup for register page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,11 @@
+import { Input } from "@heroui/react";
+
 export default function RegisterPage() {
     return (
-        <div>
-            <h1>Register Page</h1>
+        <div className="max-w-2xl m-auto flex flex-col gap-3">
+                <h1 className="text-center text-3xl font-bold mb-1">Register Page</h1>
+                <Input label="Name" type="text" variant="flat" size="sm"/>
+                <Input label="Email" type="email" variant="flat" size="sm"/>
         </div>
     )
 }
