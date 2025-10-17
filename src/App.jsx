90b1c68e7e94532261e9d14228572f72663401db
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-17 22:41:13 +0300

    added routing and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout/AuthLayout'
+import LoginPage from './pages/LoginPage/LoginPage'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
+import MainLayout from './layouts/MainLayout/MainLayout'
+import FeedPage from './pages/FeedPage/FeedPage'
+import PostDetails from './pages/PostDetails/PostDetails'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '', element: AuthLayout, children: [
+        { path: 'login', element: LoginPage },
+        { path: 'register', element: RegisterPage }
+      ]
+    },
+    {
+      path: '', element: MainLayout, children: [
+        { index: true, element: FeedPage },
+        { path: 'post-details', element: PostDetails },
+        { path: 'profile', element: ProfilePage }
+      ]
+    },
+    { path: '*', element: NotFoundPage }
+  ])
 
   return (
     <>
-      <h1>Test App</h1>
-      <h1 class="text-3xl font-bold underline">
-        Test Tailwind
-      </h1>
-      <i class="fa-solid fa-house"></i>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
