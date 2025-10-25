66118bf77e5cf66850cfe06ee0ea98cf3f036050
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-25 21:22:59 +0300

    added auth schema in dif file for better structure

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,49 +1,10 @@
 import { Input, Form, Button, Select, SelectItem } from "@heroui/react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
-import * as zod from "zod"
+import { registerSchema } from "../../schemas/auth-schema";
 
 export default function RegisterPage() {
     const genderOptions = ["Male", "Female"];
-
-    const schema = zod.object({
-        name: zod.string()
-            .nonempty("Please enter your name")
-            .min(3, { message: "Name must be at least 3 characters" })
-            .max(20, { message: "Name must be at most 20 characters" })
-            .regex(/^[a-zA-Z\s]+$/, { message: "Name must contain only letters and spaces" }),
-
-        email: zod.string()
-            .nonempty("Please enter your email")
-            .email({ message: "Please enter a valid email" }),
-
-        password: zod.string()
-            .nonempty("Please enter your password")
-            .min(8, { message: "Password must be at least 8 characters" })
-            .max(20, { message: "Password must be at most 20 characters" })
-            .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/, { message: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character" }),
-
-        rePassword: zod.string()
-            .nonempty("Please confirm your password"),
-
-        dateOfBirth: zod.coerce.date()
-            .refine((date) => !Number.isNaN(date?.getTime?.()), {
-                message: "Please enter your date of birth"
-            })
-            .refine((date) => {
-                const today = new Date();
-                const age = today.getFullYear() - date.getFullYear();
-                return age >= 18;
-            }, {
-                message: "You must be at least 18 years old"
-            }),
-
-        gender: zod.string()
-            .nonempty("Please select your gender")
-    }).refine((data) => data.password === data.rePassword, {
-        message: "Passwords do not match",
-        path: ["rePassword"]
-    })
 
     const {
         handleSubmit,
@@ -57,7 +18,7 @@ export default function RegisterPage() {
             rePassword: "",
             dateOfBirth: "",
             gender: ""
-        }, resolver: zodResolver(schema)
+        }, resolver: zodResolver(registerSchema)
     });
 
 
